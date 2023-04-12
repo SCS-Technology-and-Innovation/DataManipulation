50eaacb8eb2c11dc971640667ebe60376b99ad11
satuelisa <elisa.schaeffer@gmail.com>
2023-04-12 12:31:53 -0400

    Plans added

--- enrolment.py
+++ enrolment.py
@@ -21,6 +21,9 @@ plt.rcParams['xtick.color'] = 'white'
 plt.rcParams['ytick.color'] = 'white'
 
 values.plot.area(cmap = 'Paired', linewidth = 0)
+
+#plt.grid(color = 'darkgray', zorder = 0)
+
 plt.legend(bbox_to_anchor = (1.05, 1), loc = 'upper right', borderaxespad = 0)
 
 covid = dt.datetime(2020, 3, 1)
@@ -32,25 +35,42 @@ future = dt.datetime(2023, 6, 1)
 past = dt.datetime(2015, 1, 1)
 
 roffset = dt.timedelta(days = 12)
-loffset = dt.timedelta(days = 65)
+loffset = dt.timedelta(days = 85)
+
+fact = 'gray'
+plan = 'red'
 
 plt.axvline(x = covid, color = 'pink', alpha = 0.5, linewidth = 4)
-plt.text(covid - loffset, 300, 'Start of COVID-19', rotation = 90)
+plt.text(covid - loffset, 300, 'Start of COVID-19', rotation = 90, color = fact)
 
 plt.axvline(x = scaleai, color = 'cyan', alpha = 0.5, linewidth = 4)
-plt.text(scaleai + roffset, 250, 'ScaleAI begins', rotation = -90)
+plt.text(scaleai + roffset, 250, 'ScaleAI begins', rotation = -90, color = fact)
 
 plt.axvline(x = pratic, color = 'purple', alpha = 0.5, linewidth = 4)
-plt.text(pratic + roffset, 200, 'PRATIC for credit', rotation = -90)
+plt.text(pratic + roffset, 200, 'PRATIC credit', rotation = -90, color = fact)
 
 plt.axvline(x = pratic2, color = 'orange', alpha = 0.5, linewidth = 4)
-plt.text(pratic2 + roffset, 400, 'PRATIC for non-credit', rotation = -90)
+plt.text(pratic2 - loffset, 300, 'PRATIC non-credit', rotation = 90, color = fact)
 
-plt.axvline(x = present, color = 'gray', alpha = 0.5, linewidth = 10)
-plt.text(future, 400, 'Forecast', rotation = 0, fontsize = 24)
-plt.text(past, 300, 'Past enrolment data', rotation = 0, fontsize = 24)
+plt.axvline(x = present, color = 'gray', alpha = 0.5, linewidth = 16)
+plt.text(future, 400, 'Forecast', rotation = 0, fontsize = 16)
+plt.text(present - 3 * roffset, 400, 'NOW', rotation = 90, fontsize = 12)
+plt.text(past, 300, 'Past enrolment data', rotation = 0, fontsize = 20)
 
-plt.title('Technology & Innovation')
+ba = dt.datetime(2026, 5, 1)
+cit = dt.datetime(2025, 5, 1)
+aai = dt.datetime(2025, 6, 1) 
+
+plt.axvline(x = ba, color = 'green', alpha = 0.5, linewidth = 3)
+plt.text(ba - loffset, 300, 'BA revised', rotation = 90, color = plan)
+
+plt.axvline(x = cit, color = 'yellow', alpha = 0.5, linewidth = 3)
+plt.text(cit - loffset, 300, 'CIT modular', rotation = 90, color = plan)
+
+plt.axvline(x = aai, color = 'green', alpha = 0.5, linewidth = 3)
+plt.text(aai + roffset, 370, 'AAI online', rotation = -90, color = plan)
+
+plt.title('Technology & Innovation', fontsize = 40)
 plt.xlabel('Term')
 plt.ylabel('Registrations')
 
